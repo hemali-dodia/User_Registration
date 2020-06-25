a856f5ae8de355cc87b6e76cf84f426e87d3599a
hemali <hemalidodia1310@gmail.com>
2020-06-25 14:26:10 +0530

    validate mobile number

--- user_registration.sh
+++ user_registration.sh
@@ -32,3 +32,18 @@ function email_varification()
 }
 read -p "enter your email: " email
 email_varification $email
+
+function ValMobileNumber(){
+        local CC=$1
+        local Num=$2
+        CodePat="^[0-9+]{3}$"
+        NumPat="^[0-9]{10}$"
+        if [[ $CC =~ $CodePat && $Num =~ $NumPat ]]
+        then
+                echo "valid mobile number"
+        else
+                echo "invalid mobile number"
+        fi
+}
+read -p "enter mob number with country code: " Code Number;
+mob_num="$( ValMobileNumber $Code $Number )"
